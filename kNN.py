04275302d06ffb9c17246bdc8777b37e0a3dcebe
OATOMO <443437061@qq.com>
2018-06-06 23:24:07 +0800

    complate kNN

--- kNN.py
+++ kNN.py
@@ -17,21 +17,21 @@ def createDataSet():
 '''
 def classify0(inX,dataSet,labels,k):
     dataSetSize = dataSet.shape[0]                    #dataSet的列数
-    print("\ndataSetSize :\n",dataSetSize)
+    # print("\ndataSetSize :\n",dataSetSize)
     diffMat = tile(inX,(dataSetSize,1)) - dataSet   
-    print("\ndiffMat :\n",diffMat)
+    # print("\ndiffMat :\n",diffMat)
     sqDiffMat = (diffMat**2)
     sqDistances = sqDiffMat.sum(axis=1)             #axis＝1表示按照行的方向相加  
     distances = sqDistances**0.5                    #开方
     sortedDistIndicies = distances.argsort()
-    print("\nsortedDistIndicies :\n",sortedDistIndicies)
+    # print("\nsortedDistIndicies :\n",sortedDistIndicies)
     classCount={}
     for i in range(k):
         voteIlabel = labels[sortedDistIndicies[i]]
         classCount[voteIlabel] = classCount.get(voteIlabel,0)+1
-        print("\nclassCount :\n",classCount)
+        #print("\nclassCount :\n",classCount)
     sortedClassCount = sorted(classCount.items(),key=operator.itemgetter(1),reverse=True) #从大到小排序classCount第1个域的值
-    print("\nsortedClassCount :\n",sortedClassCount)
+    #print("\nsortedClassCount :\n",sortedClassCount)
     return sortedClassCount[0][0]
 
 def file2matrix(filename):
@@ -50,6 +50,7 @@ def file2matrix(filename):
     return returnMat,classLabelVector 
 
 
+
 # newValue = (oldValue-min)/(max-min) (归一化 0 - 1)
 def autoNorm(dataSet):
     """自动将数字特征值转化到0到1之间"""
@@ -61,3 +62,40 @@ def autoNorm(dataSet):
     normDataSet = dataSet - tile(minVals,(m,1))
     normDataSet = normDataSet/tile(ranges,(m,1))
     return normDataSet,ranges,minVals
+
+
+def datingClassTest():
+    """测试分类器"""
+    hoRatio = 0.10
+    datingDataMat,datingLabels = file2matrix('/opt/git_Atom/study_ml/Data/Ch02/datingTestSet2.txt')
+    normMat,ranges,minvals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m*hoRatio)
+    errorCount = 0.0
+    for i in range(numTestVecs):
+        classifierResult = classify0(normMat[i,:],normMat[numTestVecs:m,:],datingLabels[numTestVecs:m],4)
+        print ("the classifier came back with :%d ,the real answer is:%d\n"%(classifierResult,datingLabels[i]))
+        if(classifierResult != datingLabels[i]):
+            errorCount += 1.0
+    print ("the total error rate is %f\n"%(errorCount/float(numTestVecs)))
+
+def classifyPerson():
+    resultList = ['not at all','in small doses','in large doses']
+    percentTats = float(input("percentage of time spent playing video games?"))
+    ffmiles = float(input("frequent filer miles earned per year?"))
+    iceCream = float(input("liters of ice cream consumed per year?"))
+    datingDataMat,datingLabels = file2matrix('/opt/git_Atom/study_ml/Data/Ch02/datingTestSet2.txt')
+    normMat,ranges,minvals = autoNorm(datingDataMat)
+    inArr = array([ffmiles,percentTats,iceCream])
+    classifierResult = classify0((inArr-minvals)/ranges,normMat,datingLabels,3)
+    print ("You will probably like this person :",resultList[classifierResult-1])
+
+#手写数字识别
+def img2vector(filename):
+    returnVect = zeros((1,1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0,32*i+j] = int(lineStr[j])
+    return returnVect
