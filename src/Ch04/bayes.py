8b25ab859526063ea9397cc6dcf13c1b296e27c6
OATOMO <443437061@qq.com>
2018-06-19 17:27:59 +0800

    start bayes

---
+++ src/Ch04/bayes.py
@@ -0,0 +1,5 @@
+def loadDataSet():
+	"""词表到向量的转换函数"""
+	postingList = [['my','dog','has','flea','problems','help','please'],
+				   ['maybe','not','take','him','to','dog','park','stupid'],
+				   []]
